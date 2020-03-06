327c4a2c8fb127fb03ef5f006c7a2702ec5fafd1
iavivai <18yukitaka@gmail.com>
2020-03-07 00:59:32 +0900

    Refactoring MovieCrawler.

--- lib/movie_crawler.rb
+++ lib/movie_crawler.rb
@@ -10,11 +10,19 @@ class MovieCrawler
 
   attr_reader :lists
 
-  def run
+  def run!
     doc = Nokogiri::HTML(URI.open("#{BASE_URL}/upcoming"))
 
     doc.xpath('//div[@class="content-main"]/section//h3/a').each do |item|
       @lists[item.text] = { link: BASE_URL + item[:href] }
     end
+  end
+
+  def defalut_text
+    list_text = @lists.map do |key, value|
+      "#{key}: #{value[:link]}"
+    end.join("\n")
+
+    "今週公開する映画はこちらです😊\n#{list_text}"
   end
 end
--- lib/webhook.rb
+++ lib/webhook.rb
@@ -3,16 +3,9 @@ require 'dotenv/load'
 require './lib/movie_crawler'
 
 movie_crawler = MovieCrawler.new
-movie_crawler.run
-
-text = movie_crawler.lists.map do |key, value|
-  "#{key}: #{value[:link]}"
-end.join("\n")
+movie_crawler.run!
 
 client = Discordrb::Webhooks::Client.new(url: ENV['WEBHOOK_URL'])
 client.execute do |builder|
-  builder.content = <<~TEXT
-    今週公開する映画はこちらです😊
-    #{text}
-  TEXT
+  builder.content = movie_crawler.defalut_text
 end
