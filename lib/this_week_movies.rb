f7614f3c6b8cf6882b73e79ab586b2bb98ce94d9
iavivai <18yukitaka@gmail.com>
2020-06-13 14:12:11 +0900

    Fix bug.

--- lib/this_week_movies.rb
+++ lib/this_week_movies.rb
@@ -8,20 +8,23 @@ movie_crawler = MovieCrawler.new
 movie_crawler.run!
 
 client = Discordrb::Webhooks::Client.new(url: ENV['WEBHOOK_URL'])
-client.execute do |builder|
-  builder.content = if movie_crawler.lists.empty?
-                      '今週公開する映画はありませんでした🥺'
-                    else
-                      '今週公開する映画はこちらです😊'
-                    end
 
-  movie_crawler.lists.each do |movie|
-    builder.add_embed do |embed|
-      embed.title = movie.title
-      embed.description = movie.description
-      embed.url = movie.official_site_url
-      embed.image = Discordrb::Webhooks::EmbedImage.new(url: movie.image_url)
-      embed.timestamp = Time.now
+if movie_crawler.lists.empty?
+  client.execute { |b| b.content = '今週公開する映画はありませんでした🥺' }
+else
+  client.execute { |b| b.content = '今週公開する映画はこちらです😊' }
+
+  movie_crawler.lists.each_slice(10) do |movies|
+    client.execute do |builder|
+      movies.each do |movie|
+        builder.add_embed do |embed|
+          embed.title = movie.title
+          embed.description = movie.description
+          embed.url = movie.official_site_url
+          embed.image = Discordrb::Webhooks::EmbedImage.new(url: movie.image_url)
+          embed.timestamp = Time.now
+        end
+      end
     end
   end
 end
